7d3bafb05e73aaa37381272548a9a7f2b1baf1b9
yonghai.yang <yangyonghai9125@hotmail.com>
2019-09-17 17:53:57 +0800

    less 编译成功

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@
  * Created by yang on 2019/9/10.
  */
 import _ from 'lodash';
+// import './style.css';
+import './less.less';
+
 function component() {
   var element = document.createElement('div');
 
   // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
 
   return element;
 }
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+/**
+ * Created by yang on 2019/9/17.
+ */
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.less$/,
+        use: [{
+          loader: "style-loader" // creates style nodes from JS strings
+        }, {
+          loader: "css-loader" // translates CSS into CommonJS
+        }, {
+          loader: "less-loader" // compiles Less to CSS
+        }]
+      }
+    ]
+  }
+};
